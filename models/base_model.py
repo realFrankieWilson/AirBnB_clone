24536c79caf447f3871f8cce58df1d39a9f2d351
Gold Israel <techiegold101@gmail.com>
2023-10-11 09:10:25 +0100

    Add a method "to_dict" that returns the dictionary representation of an instance

--- models/base_model.py
+++ models/base_model.py
@@ -15,9 +15,17 @@ class BaseModel():
 		self.updated_at = datetime.now()
 
 	def __str__(self):
-		""" Return a string representation of the class instance. """
+		""" Returns a string representation of the instance. """
 		return '{[]} {()} {}'.format(self.__class__.__name__, self.id, self.__dict__)
 	
 	def save(self):
 		""""Update the updated_at attribute with the current datetime."""
 		self.updated_at = datetime.now()
+	
+	def to_dict(self):
+		"""Returns a dictionary representation of the instance."""
+		dict_obj = self.__dict__.copy()
+		dict_obj['__class__'] = self.__class__.__name__
+		dict_obj['created_at'] = self.created_at.isoformat()
+		dict_obj['updated_at'] = self.updated_at.isoformat()
+		return dict_obj
