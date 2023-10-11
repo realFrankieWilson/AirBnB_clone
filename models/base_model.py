6e341d1e3fc4b34c42877421cbbd2a1ce0da01f6
Gold Israel <techiegold101@gmail.com>
2023-10-11 09:04:19 +0100

    Add docstrings to method, I also implement the save method

--- models/base_model.py
+++ models/base_model.py
@@ -9,17 +9,15 @@ class BaseModel():
 	"""Defines all common attributes/methods for other classes."""
 
 	def __init__(self, id=''):
+		""" Initialize a new instance of the BaseModel """
 		self.id = str(uuid4())
 		self.created_at = datetime.now()
 		self.updated_at = datetime.now()
 
 	def __str__(self):
-		return '{[]} {()} {}'.format(BaseModel.__name__, self.id, self.__dict__)
-
+		""" Return a string representation of the class instance. """
+		return '{[]} {()} {}'.format(self.__class__.__name__, self.id, self.__dict__)
+	
 	def save(self):
-
-
-
-
-my_obj = BaseModel(123, 
-print(my_obj.id)
+		""""Update the updated_at attribute with the current datetime."""
+		self.updated_at = datetime.now()
