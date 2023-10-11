a8577fdde04c75a2b43d46a299d293cc90d25fd2
Gold Israel <techiegold101@gmail.com>
2023-10-11 09:12:30 +0100

    implement pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -5,27 +5,29 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel():
-	"""Defines all common attributes/methods for other classes."""
+    """Defines all common attributes/methods for other classes."""
 
-	def __init__(self, id=''):
-		""" Initialize a new instance of the BaseModel """
-		self.id = str(uuid4())
-		self.created_at = datetime.now()
-		self.updated_at = datetime.now()
+    def __init__(self, id=''):
+        """ Initialize a new instance of the BaseModel """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-	def __str__(self):
-		""" Returns a string representation of the instance. """
-		return '{[]} {()} {}'.format(self.__class__.__name__, self.id, self.__dict__)
-	
-	def save(self):
-		""""Update the updated_at attribute with the current datetime."""
-		self.updated_at = datetime.now()
-	
-	def to_dict(self):
-		"""Returns a dictionary representation of the instance."""
-		dict_obj = self.__dict__.copy()
-		dict_obj['__class__'] = self.__class__.__name__
-		dict_obj['created_at'] = self.created_at.isoformat()
-		dict_obj['updated_at'] = self.updated_at.isoformat()
-		return dict_obj
+    def __str__(self):
+        """ Returns a string representation of the instance. """
+        return '{[]} {()} {}'.format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """"Update the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary representation of the instance."""
+        dict_obj = self.__dict__.copy()
+        dict_obj['__class__'] = self.__class__.__name__
+        dict_obj['created_at'] = self.created_at.isoformat()
+        dict_obj['updated_at'] = self.updated_at.isoformat()
+        return dict_obj
