427d31566ff922d5c3fede5932d07957c5a9854b
Gold Israel <techiegold101@gmail.com>
2023-10-12 16:20:05 +0100

    Add test cases for FileStorage class

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,24 @@
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+class TestFileStorage(unittest.TestCase):
+    def test_save_and_reload(self):
+        storage = FileStorage()
+        obj = BaseModel()
+        storage.new(obj)
+        storage.save()
+        
+        # create a new storage instance and reload objects.
+        new_storage = FileStorage()
+        new_storage.reload()
+        
+        self.assertEqual(len(new_storage.all()), 1)
+        self.assertEqual(new_storage.all()
+                         [f"{obj.__class__.__name__}.{obj.id}"])
+        self.assertEqual(obj.to_dict(), new_storage.all()
+                         [f"{obj.__class__.__name__}.{obj.id}"].to_dict())
+    
+    
+if __name__ == '__main__':
+    unittest.main()      
