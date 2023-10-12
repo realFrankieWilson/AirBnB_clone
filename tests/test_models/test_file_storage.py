91d3b087d8e88fbf93b1fd6e0db812cb4168f409
Gold Israel <techiegold101@gmail.com>
2023-10-12 19:02:20 +0100

    update test for FileStorage class

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -1,24 +1,64 @@
 import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+import os
 
 
 class TestFileStorage(unittest.TestCase):
-    def test_save_and_reload(self):
-        storage = FileStorage()
-        obj = BaseModel()
-        storage.new(obj)
-        storage.save()
+    """
+    Test cases for the FileStorage class.
+    """
 
-        # create a new storage instance and reload objects.
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up for the test class.
+        """
+        cls.storage = FileStorage()
+        cls.obj = BaseModel()
+        cls.obj.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Clean up after the test class.
+        """
+        os.remove("file.json")
+
+    def test_all(self):
+        """
+        Test the all() method of FileStorage.
+        """
+        objects = self.storage.all()
+        self.assertTrue(isinstance(objects, dict))
+        self.assertIn("BaseModel.{}".format(self.obj.id), objects)
+
+    def test_new(self):
+        """
+        Test the new() method of FileStorage.
+        """
+        new_obj = BaseModel()
+        self.storage.new(new_obj)
+        objects = self.storage.all()
+        self.assertIn("BaseModel.{}".format(new_obj.id), objects)
+
+    def test_save(self):
+        """
+        Test the save() method of FileStorage.
+        """
+        new_obj = BaseModel()
+        new_obj.save()
+        objects = self.storage.all()
+        self.assertIn("BaseModel.{}".format(new_obj.id), objects)
+
+    def test_reload(self):
+        """
+        Test the reload() method of FileStorage.
+        """
         new_storage = FileStorage()
         new_storage.reload()
-
-        self.assertEqual(len(new_storage.all()), 1)
-        self.assertEqual(new_storage.all()
-                         [f"{obj.__class__.__name__}.{obj.id}"])
-        self.assertEqual(obj.to_dict(), new_storage.all()
-                         [f"{obj.__class__.__name__}.{obj.id}"].to_dict())
+        objects = new_storage.all()
+        self.assertIn("BaseModel.{}".format(self.obj.id), objects)
 
 
 if __name__ == '__main__':
