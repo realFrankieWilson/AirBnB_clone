b601d67f00760a474a51238caf44530b692a247d
Gold Israel <techiegold101@gmail.com>
2023-10-13 11:39:16 +0100

    Add review  class with its functionality

---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This module contains the Review class (Child-class to BaseModel) """
+from models.base_model import BaseModel
+
+class Review (BaseModel):
+    """
+    Implement the Review module
+    
+    Args:
+       place_id(str): place id.
+       user_id(str): user id.
+       test(str): user review.
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
