c445da08af855f68bb29b89c6548fe0f769bd504
Gold Israel <techiegold101@gmail.com>
2023-10-13 10:02:31 +0100

    I link  BaseModel to FileStorage by using the variable storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,63 +1,67 @@
 #!/usr/bin/python3
-# base_model.py
+"""
+A module that implements the BaseModel class
+"""
 
 # modules importation.
 from uuid import uuid4
 from datetime import datetime
 
 
+
 class BaseModel:
-    """Defines all common attributes/methods for other classes."""
+    """
+    A class that defines all common attributes/methods for other classes
+    """
 
-    def _init_(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
-        Initialize a new instance of the BaseModel
-        args:
-            args => a tuple of arguments.
-            kwargs => key-values arguments (arguments)
+        Initialize the BaseModel class
         """
 
-        if kwargs:
-            # stores the datetime format
-            datetime_format = "%Y-%m-%dT%H:%M:%S.%f"
-            for key in kwargs:
-                if key == 'created_at':
-                    self._dict_['created_at'] = datetime.strptime(
-                        kwargs['created_at'], datetime_format
-                        )
-                elif key == 'updated_at':
-                    self._dict_['updated_at'] = datetime.strptime(
-                        kwargs['updated_at'], datetime_format
-                        )
-                else:
-                    self._dict_[key] = kwargs[key]
-        else:
+        from models import storage
+        if not kwargs:
             self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
 
-    def _str_(self):
-        """ Returns a string representation of the instance. """
-        return '[{}] ({}) {}'.format(self._class.name_,
-                                     self.id, self._dict_)
+    def __str__(self):
+        """
+        Returns the string representation of BaseModel object.
+        [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(type(self).__name__, self.id,
+                                     self.__dict__)
 
     def save(self):
-        """"Update the updated_at attribute with the current datetime."""
+        """
+        Updates 'self.updated_at' with the current datetime
+        """
+        from models import storage
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of
-        _dict_ of the instance.
-                by using self._dict_, only instance attributes
-                set will be returned.
-                a key _class_ must be added to this dictionary
-                with the class name of the object.
-                created_at and updated_at must be converted
-                to string object in ISO format.
+        returns a dictionary containing all keys/values of __dict__
+        of the instance:
+
+        - only instance attributes set will be returned
+        - a key __class__ is added with the class name of the object
+        - created_at and updated_at must be converted to string object in ISO
+        object
         """
-        dict_obj = self._dict_.copy()
-        dict_obj['_class'] = self.__class__.___name__
-        dict_obj['created_at'] = self.created_at.isoformat()
-        dict_obj['updated_at'] = self.updated_at.isoformat()
-        return dict_obj
+        dict_1 = self.__dict__.copy()
+        dict_1["__class__"] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if k in ("created_at", "updated_at"):
+                v = self.__dict__[k].isoformat()
+                dict_1[k] = v
+        return dict_1
